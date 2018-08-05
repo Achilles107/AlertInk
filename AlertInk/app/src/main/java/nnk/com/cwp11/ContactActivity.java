53a8f23565d6d16744b4d017256d60225bf2f019
Achilles107 <Achilles107@users.noreply.github.com>
2018-08-05 12:44:56 +0530

    Dialog and Calender Added

--- AlertInk/app/src/main/java/nnk/com/cwp11/ContactActivity.java
+++ AlertInk/app/src/main/java/nnk/com/cwp11/ContactActivity.java
@@ -1,5 +1,7 @@
 package nnk.com.cwp11;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.database.Cursor;
@@ -8,12 +10,18 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -26,6 +34,12 @@ public class ContactActivity extends AppCompatActivity
     private static ContactAdapter contactAdapter;
     private static ProgressDialog progressDialog;
     private static AlertDialog.Builder builder;
+    private static Button button;
+    private static LayoutInflater layoutInflater1;
+    private static AlertDialog alertDialog;
+    int day,month,year;
+    Calendar calendar;
+    private static TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,20 +47,47 @@ public class ContactActivity extends AppCompatActivity
         getSupportActionBar().setTitle("Select Contact");
 
         listView=(ListView)findViewById(R.id.list_contacts);
-        //
+
         new LoadContacts().execute();
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-            {
-                ContactModel model=list.get(position);
-                builder=new AlertDialog.Builder(ContactActivity.this);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ContactModel model = list.get(position);
+                builder = new AlertDialog.Builder(ContactActivity.this);
+                layoutInflater1 = getLayoutInflater();
+                View dview = layoutInflater1.inflate(R.layout.inside_dialog, null);
                 builder.setTitle(model.getName());
                 builder.setMessage(model.getPhone_number());
+                builder.setView(dview);
+                textView = (TextView) dview.findViewById(R.id.date);
+                button = (Button) dview.findViewById(R.id.set_date);
+                alertDialog = builder.create();
+                textView.setText("dd/mm/yyyy");
+                calendar = Calendar.getInstance();
+                day = calendar.get(Calendar.DAY_OF_MONTH);
+                month = calendar.get(Calendar.MONTH);
+                year = calendar.get(Calendar.YEAR);
+                showDate(year,month+1,day);
+                //Dialog d=new Dialog(ContactActivity.this);
+                //button=(Button)d.findViewById(R.id.set_date);
+                /*button.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.cancel();
+                        Toast.makeText(getApplicationContext(),"Set",Toast.LENGTH_SHORT).show();
+                    }
+                });*/
+                //builder.setMessage("Set Birth date");
                 builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(), "Added", Toast.LENGTH_SHORT).show();
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        Toast.makeText(getApplicationContext(),textView.getText().toString()+"",Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                        /*calendar = Calendar.getInstance();
+                        day = calendar.get(Calendar.DAY_OF_MONTH);
+                        month = calendar.get(Calendar.MONTH);
+                        year = calendar.get(Calendar.YEAR);*/
                     }
                 });
                 builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -102,5 +143,35 @@ public class ContactActivity extends AppCompatActivity
             list.add(contactModel);
         }
         phones.close();
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setDate(View view)
+    {
+        showDialog(999);
+        //Toast.makeText(ContactActivity.this, "HI", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        if (id==999)
+        {
+            return new DatePickerDialog(ContactActivity.this,myDateListener,year,month,day);
+        }
+        return null;
+    }
+
+    private DatePickerDialog.OnDateSetListener myDateListener=new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)
+        {
+            showDate(year,monthOfYear+1,dayOfMonth);
+
+        }
+    };
+
+    public void showDate(int year,int month,int day)
+    {
+        textView.setText(new StringBuilder().append(day).append("/").append(month).append("/").append(year));
     }
 }
